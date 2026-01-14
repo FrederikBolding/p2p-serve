92d0aca433e304458bdf62bf7795389373307053
Frederik Bolding <frederik.bolding@gmail.com>
2026-01-14 01:36:53 +0100

    Fix formatting

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import process from 'node:process'; 
+import process from "node:process";
 import { handleClient } from "./client";
-import { handleServer } from './server';
-
+import { handleServer } from "./server";
 
 async function main() {
   const input = process.argv.slice(2);
   const mode = input[0];
   const portOrPublicKey = input[1];
 
-  if (mode === 'client') {
+  if (mode === "client") {
     return handleClient(portOrPublicKey);
   }
 
